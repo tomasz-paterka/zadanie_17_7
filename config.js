39ee5eeae394231be09c91667f65fb2b3f6900d7
Tomasz Paterka <tomekpaterka@gmail.com>
2018-04-14 20:55:28 +0200

    Initial

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '498655579656-mrlbm2at8553emlat7g507cu0qj1e5fj.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'gSHW0z4OwZfnYKs3o1OojXaX',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
